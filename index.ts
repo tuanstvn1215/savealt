61a0f1e5845dcf56cb80a2992d28a7e978593cc7
tuanstvn1215 <39292548+tuanstvn1215@users.noreply.github.com>
2022-10-31 11:04:50 +0700

    init 4

--- index.ts
+++ index.ts
@@ -28,6 +28,24 @@ app.post("/save", async (req: Request, res: Response) => {
     res.send("hello");
   } catch (error) {}
 });
+app.use(function(req, res, next) {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
